56380fb7946904bd744723578ab4c0d6a3b20c3e
heistermann <maik.heistermann@uni-potsdam.de>
2018-08-21 11:27:33 +0000

    Minor bug fix and readme template

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# tprof2flux
-Computing groundwater flux as sediment surface from vertical temperature profiles
+# tprof2flux: Computing groundwater flux as sediment surface from vertical temperature profiles
+
+Quick and dirty implementation based on Schmidt et al. (2016).
+
+## Content
+
+tprof2flux.R: Implementation in R
+
+T-Messungen.csv: Sample data #1
+
+T-Messungen2.csv: Sample data #2
+
+tprof2flux.xlsx: Reference implementation in Excel with Solver
+
+
+## References
+
+Schmidt, C., Bayer-Raich, M., and Schirmer, M.: Characterization of spatial heterogeneity of groundwater-stream water interactions using multiple depth streambed temperature measurements at the reach scale, Hydrol. Earth Syst. Sci., 10, 849-859, https://doi.org/10.5194/hess-10-849-2006, 2006.
--- tprof2flux.R
+++ tprof2flux.R
@@ -1,11 +1,12 @@
 # R-SKRIPT ZUR ANPASSUNG VON GW-FLIESSRATEN AN T-PROFILE
 #    NACH SCHMIDT ET AL. (2006)
 
+# Session -> Set Working Directory -> To Source File Location
 
 # Lese Datei mit gemessenen T-Profilen
 #   Messtiefen (in cm!!) in den Spaltenueberschriften
 Tmess = read.table("T-Messungen.csv", header=T, 
-                    na.strings="-9999", sep=";")
+                   na.strings="-9999", sep=";")
 
 # Fehlermass (RMSE), das minimiert wird
 rmse = function(x1,x2) {
@@ -22,31 +23,35 @@ c_f = 4190   # Spezifische Waermekapazitaet von Wasser (J/kg/K)
 K_fs = 2     # Waermeleitfaehigkeit des gesaettigten Sediments (J/s/m/K)
 L = 10       # Tiefe ab welcher GW-Temperatur als konstant angenommen wird (m)
 
-# Moeglicher Wertebereich fuer q_z (fuer die Optimierung)
+# Moeglicher Wertebereich fuer q_z
+#   (fuer die Optimierung ggf. anpassen)
+#   und willkuerlicher Startwert
 lower = -1.e-4
 upper = -1.e-8
+param = c(q_z = -1.e-7)
 
 # Vektor der Messtiefen (m) gemaess Spaltennamen
-tiefen = as.numeric(gsub("z", "", names(Tmess)[4:ncol(Tmess)])) / 100.
+tiefen = as.numeric(gsub("z", "", 
+                         names(Tmess)[4:ncol(Tmess)])) / 100.
 ix  = 4:(length(tiefen) + 3)
 
 # Modellgleichung fuer stationaeres T-Profil (nach Schmidt et al. 2006, Gl. 2)
 Tmod = function(p, z, T0, L) {
   if (p["q_z"]==0) {
-      return( z*(TL-T0)+T0 ) 
-    } else {
-        return(
-          ( exp(p["q_z"] * rho_f * c_f * z / K_fs) - 1 ) * (TL - T0) /
-            ( exp(p["q_z"] * rho_f * c_f * L / K_fs) - 1 ) + T0
-        )
-      }
-    }
+    return( z*(TL-T0)+T0 ) 
+  } else {
+    return(
+      ( exp(p["q_z"] * rho_f * c_f * z / K_fs) - 1 ) * (TL - T0) /
+        ( exp(p["q_z"] * rho_f * c_f * L / K_fs) - 1 ) + T0
+    )
+  }
+}
 
 # Nun wird fuer jedes gemessene T-Profil das optimale q_z gesucht
 result = data.frame()
 for (i in 1:nrow(Tmess)) {
   T0 = Tmess$z0[i]
-
+  
   Temp = which(!is.na(Tmess[i,ix]))+3
   depth = tiefen[which(!is.na(Tmess[i,ix]))]
   minfun <- function(p) rmse(Tmess[i,Temp],
@@ -67,9 +72,7 @@ result
 
 # Plotten der Ergebnisse
 windows()
-
 op <- par(mfrow=c(5,5), mar = c(2,2,2,2))
-
 for (i in 1:nrow(Tmess)) {
   tns=result$TNR[i]
   shore_dist=result$WEITE[i]
@@ -80,6 +83,5 @@ for (i in 1:nrow(Tmess)) {
   lines(Tmod(p=c(q_z=result$OPTIM[i]), z=depth, T0=T0, L=L), depth, col="black", lwd=2)
   if (i==25) break
 }
-
 par(op)
 
